eeb872620fa430c2b510d55767d600b704392cbc
Luan-Leite <luan.leite@fatec.sp.gov.br>
2021-10-13 21:38:55 -0300

    upload app

---
+++ dist/precache-manifest.89f84737d713a3e92f7aed02c1f73473.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621b4635e75aa695da82",
+    "url": "/css/chunk-19842f45.4be633fa.css"
+  },
+  {
+    "revision": "8e732594acca53c28630",
+    "url": "/css/chunk-5f43e4be.be495b5a.css"
+  },
+  {
+    "revision": "101ab32938e86bb8db58",
+    "url": "/css/chunk-vendors.5f3d6b55.css"
+  },
+  {
+    "revision": "18f10517f40af903c804e150fd126c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05f3fe1c8c0a16738e7",
+    "url": "/js/app.778a96f0.js"
+  },
+  {
+    "revision": "621b4635e75aa695da82",
+    "url": "/js/chunk-19842f45.592a8cd7.js"
+  },
+  {
+    "revision": "8e732594acca53c28630",
+    "url": "/js/chunk-5f43e4be.60d7903c.js"
+  },
+  {
+    "revision": "101ab32938e86bb8db58",
+    "url": "/js/chunk-vendors.04f58ae4.js"
+  },
+  {
+    "revision": "6b33444aa09854456e754b83eb19c499",
+    "url": "/manifest.json"
+  }
+]);
